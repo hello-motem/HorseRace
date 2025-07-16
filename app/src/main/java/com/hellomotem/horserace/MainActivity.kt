c1e9d05992da878306de331b8b314fd4251f6604
superiorsofa <ms.chelokidi@mail.ru>
2025-07-16 17:03:00 +0300

    Добавил табы Гонки и Истории

--- app/src/main/java/com/hellomotem/horserace/MainActivity.kt
+++ app/src/main/java/com/hellomotem/horserace/MainActivity.kt
@@ -5,16 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayout
+import com.hellomotem.horserace.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupUi()
+    }
+
+    private fun setupUi() {
+        resources.getStringArray(R.array.tabs_array)
+            .map { tabText -> binding.tabs.newTab().also { it.setText(tabText) } }
+            .forEach(binding.tabs::addTab)
     }
 }
