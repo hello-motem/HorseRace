477bc8b94d63fdc35f63e563ecfe286917251e19
superiorsofa <ms.chelokidi@mail.ru>
2025-07-17 19:38:25 +0300

    Добавил делегат для получения массива строк

--- app/src/main/java/com/hellomotem/horserace/delegates/StringRes.kt
+++ app/src/main/java/com/hellomotem/horserace/delegates/StringRes.kt
@@ -1,8 +1,14 @@
 package com.hellomotem.horserace.delegates
 
+import android.app.Activity
+import androidx.annotation.ArrayRes
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 
 fun Fragment.stringRes(@StringRes resId: Int) = lazy(LazyThreadSafetyMode.NONE) {
     resources.getString(resId)
+}
+
+fun Activity.stringArray(@ArrayRes resId: Int) = lazy(LazyThreadSafetyMode.NONE) {
+    resources.getStringArray(resId)
 }
